cbefc5a81e2e9628fb0dede4cac139b58458d23b
Pavan <29862423+pavanyejare@users.noreply.github.com>
2022-03-18 20:45:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,10 +9,5 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
-  #  access_key = "AKIAWBFHDEMM4K76HYRC"
-  #secret_key = "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"
-  access_key = "AKIAWBFHDEMMZPSFSCO3"
-  secret_key = "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"
-
 }
 
