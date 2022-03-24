1d7d269140df474832921d3603b0c06740fabe76
Pavan Yejare <yejare.pavan@gmail.com>
2022-03-24 19:15:11 +0530

    encrypted efs volume

--- modules/efs/main.tf
+++ modules/efs/main.tf
@@ -2,6 +2,7 @@ resource "aws_efs_file_system" "efs" {
   creation_token   = var.efs_name
   performance_mode = "generalPurpose"
   throughput_mode  = "bursting"
+  encrypted        = true
   tags = {
     Name = var.efs_name
   }
